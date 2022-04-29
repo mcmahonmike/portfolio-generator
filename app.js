129400362d3f18c6372788fda9322556bda486f5
Hephaestus <mcmahonmike9@yahoo.com>
2022-04-29 18:18:33 -0500

    validation added to question prompts

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your Github Username:'
+      message: 'Enter your Github Username(Required):',
+      validate: usernameInput => {
+        if (usernameInput) {
+          return true;
+        } else {
+          console.log('Please enter your Github username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -34,12 +42,28 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project?(Required)',
+      validate: projectInput => {
+        if (projectInput) {
+          return true;
+        } else {
+          console.log('Please enter your project name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('Please provide a descrition of your project!');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -50,7 +74,15 @@ Add a New Project
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('Please enter the Github link to your project!');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
